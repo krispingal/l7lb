02c327b17768ee995402093ded9484c9d0d9a21a
Krishna Babuji <krishna.pingal@gmail.com>
2024-10-10 23:25:26 -0500

    Add zap common logging setup

---
+++ internal/infrastructure/logging.go
@@ -0,0 +1,24 @@
+package infrastructure
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Logger *zap.Logger
+
+func InitLogger() {
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	var err error
+	Logger, err = config.Build()
+	if err != nil {
+		// Fallback to standard logger in case zap initialization fails
+		os.Exit(1)
+	}
+	defer Logger.Sync() // Ensure that any buffered log entries are flushe
+}
