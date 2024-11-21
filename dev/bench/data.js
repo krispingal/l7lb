b983f80476835e131c2264e607024adb8e730377
github-action-benchmark <github@users.noreply.github.com>
2024-11-21 02:03:01 +0000

    add L7LB Go Benchmarks (go) benchmark result for 63fad0f98cb82527039d85243ab3464724634101

---
+++ dev/bench/data.js
@@ -0,0 +1,272 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1732154581296,
+  "repoUrl": "https://github.com/krispingal/l7lb",
+  "entries": {
+    "L7LB Go Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "krishna.pingal@gmail.com",
+            "name": "Krishna Babuji",
+            "username": "krispingal"
+          },
+          "committer": {
+            "email": "krishna.pingal@gmail.com",
+            "name": "Krishna Babuji",
+            "username": "krispingal"
+          },
+          "distinct": true,
+          "id": "63fad0f98cb82527039d85243ab3464724634101",
+          "message": "Fix benchmark build",
+          "timestamp": "2024-11-20T20:01:56-06:00",
+          "tree_id": "de023154838bbcd38d374e20830986323c82da98",
+          "url": "https://github.com/krispingal/l7lb/commit/63fad0f98cb82527039d85243ab3464724634101"
+        },
+        "date": 1732154580935,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3147699311,
+            "unit": "ns/op\t  214536 B/op\t     609 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3147699311,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 214536,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 609,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3182322389,
+            "unit": "ns/op\t  115072 B/op\t     508 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3182322389,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 115072,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 508,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3139438030,
+            "unit": "ns/op\t  107976 B/op\t     504 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3139438030,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 107976,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 504,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3224320936,
+            "unit": "ns/op\t   87320 B/op\t     485 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3224320936,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 87320,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 485,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3172257971,
+            "unit": "ns/op\t  126904 B/op\t     510 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3172257971,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 126904,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 510,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3138384086,
+            "unit": "ns/op\t   78728 B/op\t     491 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3138384086,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 78728,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 491,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3245115091,
+            "unit": "ns/op\t  105072 B/op\t     498 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3245115091,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 105072,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 498,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3038282005,
+            "unit": "ns/op\t  122160 B/op\t     495 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3038282005,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 122160,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 495,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3148326228,
+            "unit": "ns/op\t  107760 B/op\t     500 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3148326228,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 107760,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 500,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest",
+            "value": 3176730543,
+            "unit": "ns/op\t   80872 B/op\t     498 allocs/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - ns/op",
+            "value": 3176730543,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - B/op",
+            "value": 80872,
+            "unit": "B/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRouteRequest - allocs/op",
+            "value": 498,
+            "unit": "allocs/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
